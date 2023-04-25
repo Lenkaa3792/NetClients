b2ae49a2e8c819829fc5bcaf994e60c6f15be248
Lenkaa3792 <allanlenkaa@gmail.com>
2023-04-25 11:59:12 +0300

    routes

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,11 @@
 class ClientsController < ApplicationController
+ 
+    def index
+        customer=Client.all
+        render json: customer
+    end
+    def show
+        customer = Clients.find(params[:id])
+        render json: customer, include: ['']
+       end
 end
